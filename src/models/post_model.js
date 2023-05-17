70374ef7317675838b2868f8f3a261f0360f18cb
Bansharee Ireen <81589236+banshee56@users.noreply.github.com>
2023-05-17 13:49:28 -0400

    getting rid of fields for now

--- src/models/post_model.js
+++ src/models/post_model.js
@@ -3,13 +3,9 @@ import mongoose, { Schema } from 'mongoose';
 // create a PostSchema with a title field
 export const PostSchema = new Schema({
   title: { type: String, required: true },
-  coverUrl: { type: String, required: true },
+  coverUrl: { type: String, required: false },
   content: { type: String, required: false },
   tags: { type: String, required: false },
-  caught: { type: Boolean, required: false },
-  gender: { type: String, required: false },
-  height: { type: String, required: false },
-  weight: { type: String, required: false },
 }, {
   timestamps: true,
   toObject: { virtuals: true },
